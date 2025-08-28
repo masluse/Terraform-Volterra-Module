2b6ef4066ac4750fd88c9f09b4a2d946f27a366d
Manuel Elia Regli <manuel.regli@bison-group.com>
2025-08-28 16:17:06 +0000

    ec

--- volterra_route/main.tf
+++ volterra_route/main.tf
@@ -41,7 +41,7 @@ resource "volterra_route" "ignore_route_changes" {
     for_each = var.path_redirects
     content {
       dynamic "match" {
-        for_each = routes.value
+        for_each = routes.value.match
         content {
           http_method = "ANY"
           path {
@@ -60,6 +60,7 @@ resource "volterra_route" "ignore_route_changes" {
         path_redirect  = routes.key
         response_code  = 301
         proto_redirect = "incoming-proto"
+        remove_all_params = routes.value.params == "remove" ? true : false
       }
     }
   }
