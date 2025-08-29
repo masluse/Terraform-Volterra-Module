67a29c70af49646372a0941fad6493aa684dfa06
Manuel Elia Regli <manuel.regli@bison-group.com>
2025-08-29 08:54:36 +0000

    Edit main.tf

--- volterra_route/main.tf
+++ volterra_route/main.tf
@@ -11,13 +11,13 @@ resource "volterra_route" "default" {
         content {
           http_method = "ANY"
           path {
-            path = replace(replace(replace(replace(replace(match.value.path, " ", "%20"), "ä", "%C3%A4"), "ö", "%C3%B6"), "ü", "%C3%BC"), "é", "%C3%A9")
+            path = replace(replace(replace(replace(replace(replace(match.value.path, " ", "%20"), "ä", "%C3%A4"), "ö", "%C3%B6"), "ü", "%C3%BC"), "é", "%C3%A9"), "à", "%C3%A0")
           }
           dynamic "query_params" {
             for_each = match.value.query_params
             content {
               key   = query_params.key
-              exact = replace(replace(replace(replace(replace(query_params.value, " ", "%20"), "ä", "%C3%A4"), "ö", "%C3%B6"), "ü", "%C3%BC"), "é", "%C3%A9")
+              exact = replace(replace(replace(replace(replace(replace(query_params.value, " ", "%20"), "ä", "%C3%A4"), "ö", "%C3%B6"), "ü", "%C3%BC"), "é", "%C3%A9"), "à", "%C3%A0")
             }
           }
           dynamic "headers" {
@@ -52,13 +52,13 @@ resource "volterra_route" "ignore_route_changes" {
         content {
           http_method = "ANY"
           path {
-            path = replace(replace(replace(replace(replace(match.value.path, " ", "%20"), "ä", "%C3%A4"), "ö", "%C3%B6"), "ü", "%C3%BC"), "é", "%C3%A9")
+            path = replace(replace(replace(replace(replace(replace(match.value.path, " ", "%20"), "ä", "%C3%A4"), "ö", "%C3%B6"), "ü", "%C3%BC"), "é", "%C3%A9"), "à", "%C3%A0")
           }
           dynamic "query_params" {
             for_each = match.value.query_params
             content {
               key   = query_params.key
-              exact = replace(replace(replace(replace(replace(query_params.value, " ", "%20"), "ä", "%C3%A4"), "ö", "%C3%B6"), "ü", "%C3%BC"), "é", "%C3%A9")
+              exact = replace(replace(replace(replace(replace(replace(query_params.value, " ", "%20"), "ä", "%C3%A4"), "ö", "%C3%B6"), "ü", "%C3%BC"), "é", "%C3%A9"), "à", "%C3%A0")
             }
           }
           dynamic "headers" {
