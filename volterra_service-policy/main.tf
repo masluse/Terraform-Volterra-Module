c672a2b18e0a59ceb8f6c4a020c3cf285cbdc695
Manuel Regli <manuel.regli@bison-group.com>
2025-09-26 08:54:58 +0200

    ec

---
+++ volterra_service-policy/main.tf
@@ -0,0 +1,19 @@
+resource "volterra_service_policy" "default" {
+  name      = "sp-${var.platform}-${var.key}"
+  namespace = "nspace-${var.platform}-${var.value.namespace}"
+  algo      = "FIRST_MATCH"
+
+  // One of the arguments from this list "deny_list rule_list legacy_rule_list allow_all_requests deny_all_requests internally_generated allow_list" must be set
+
+  allow_list {
+    // One of the arguments from this list "default_action_next_policy default_action_deny default_action_allow" must be set
+    default_action_next_policy = true
+
+    prefix_list {
+      prefixes      = try(var.value.prefixes, [])
+      ipv6_prefixes = try(var.value.ipv6_prefixes, [])
+    }
+  }
+  // One of the arguments from this list "any_server server_name server_selector server_name_matcher" must be set
+  any_server = true
+}
---
+++ volterra_service-policy/variables.tf
@@ -0,0 +1,18 @@
+variable "platform" {
+  type        = string
+  description = "The platform for which the Volterra resources are being configured. This is used to differentiate between different environments or deployments."
+}
+
+variable "key" {
+  type        = string
+  description = "A unique identifier for the service policy. This is used to create a distinct name for the service policy resource."
+}
+
+variable "value" {
+  type = object({
+    namespace     = string
+    prefixes      = optional(list(string))
+    ipv6_prefixes = optional(list(string))
+  })
+  description = "An object containing the configuration values for the service policy, including the namespace and optional lists of IPv4 and IPv6 prefixes."
+}
